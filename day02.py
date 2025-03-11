085fabae768403d0bbf20afa5cf51730de47669e
Tahir2022 <your-email@example.com>
2025-03-11 21:04:33 +0100

    Pandas lab

--- day02.py
+++ day02.py
@@ -49,3 +49,39 @@ print(df_students.loc[0, "Grade"])
 df_students = pd.read_csv("grades.csv", delimiter = ",", header = "infer")
 print(df_students.head())
 print(df_students.isnull())
+print(df_students.isnull().sum())
+
+print(df_students[df_students.isnull().any(axis=1)])
+
+df_students.StudyHours = df_students.StudyHours.fillna(df_students.StudyHours.mean())
+print(df_students)
+#print(df_students.columns)
+df_students = df_students.dropna(axis=0, how="any")
+print(df_students)
+
+# Get the mean study hours using to column name as an index
+mean_study = df_students['StudyHours'].mean()
+
+# Get the mean grade using the column name as a property (just to make the point!)
+mean_grade = df_students.Grade.mean()
+
+print('Average weekly study hours :{:.2f}\nAverage grade :{:.2f}'.format(mean_study,mean_grade))
+
+# Get students who studied for the mean or more hours
+print(df_students[df_students.StudyHours > mean_study])
+
+print(df_students[df_students.StudyHours > mean_study].Grade.mean())
+
+passes = pd.Series(df_students['Grade'] >= 60)
+df_students = pd.concat([df_students, passes.rename("Pass")], axis=1)
+print(df_students)
+
+print(df_students.groupby(df_students.Pass).Name.count())
+
+print(df_students.groupby(df_students.Pass)[['StudyHours', 'Grade']].mean())
+
+# Create a DataFrame with the data sorted by Grade (descending)
+df_students = df_students.sort_values('Grade', ascending=False)
+
+# Show the DataFrame
+print(df_students)
